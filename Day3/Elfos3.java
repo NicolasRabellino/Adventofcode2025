1c0ecd413ee0e5ecb91c193a56f0f85f95e7422c
Winky <nicorabellino1008@gmail.com>
2026-01-02 18:02:32 -0300

    day3

---
+++ Day3/Elfos3.java
@@ -0,0 +1,16 @@
+package Day3;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Elfos3 {
+    public static void main(String[] args) throws Exception {
+        String[] input = Files.readAllLines(Path.of(args[0])).toArray(new String[0]);
+
+        for(String numEntero : input){
+            
+        }
+
+    }
+}
