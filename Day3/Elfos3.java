7ca3602bdbbdac940ea7573abbf7c7a3cd5238ce
Winky <nicorabellino1008@gmail.com>
2026-01-04 03:24:35 -0300

    3.2

--- Day3/Elfos3.java
+++ Day3/Elfos3.java
@@ -9,7 +9,7 @@ public class Elfos3 {
     public static void main(String[] args) throws Exception {
 
         String[] input = Files.readAllLines(Path.of(args[0])).toArray(new String[0]);
-        int joltageTotal = 0;
+        long joltageTotal = 0;
         for (String numEntero : input) {
             joltageTotal += findJoltage(numEntero);
         }
@@ -18,24 +18,50 @@ public class Elfos3 {
 
     }
 
-    public static int findJoltage(String numeroEntero) {
+    public static long findJoltage(String numeroEntero) {
 
-        int[] numeroEnteroInt = Arrays.stream(numeroEntero.split("")).mapToInt(Integer::parseInt).toArray();
+        long[] numeroEnteroInt = Arrays.stream(numeroEntero.split("")).mapToLong(Integer::parseInt).toArray();
+
+        /*
+   ------------------------------------------- 3.1 ----------------------------------------------------------------------
 
         int numA = 0, numB = 0, indexNumA = 0;
-        for(int i = 0; i < numeroEnteroInt.length-1; i++) {
-            if(numeroEnteroInt[i] > numA){
+        for (int i = 0; i < numeroEnteroInt.length - 1; i++) {
+            if (numeroEnteroInt[i] > numA) {
                 numA = numeroEnteroInt[i];
                 indexNumA = i;
             }
         }
 
-        for(int i = indexNumA + 1; i < numeroEnteroInt.length; i++) {
-            if(numeroEnteroInt[i] > numB){
+        for (int i = indexNumA + 1; i < numeroEnteroInt.length; i++) {
+            if (numeroEnteroInt[i] > numB) {
                 numB = numeroEnteroInt[i];
             }
         }
 
-        return numA * 10 + numB;
+    ------------------------------------------ 3.1 ----------------------------------------------------------------------
+       */
+
+
+        long result = 0;
+        int auxIndex = 0;
+
+        for (int i = 0; i < 12; i++) {
+            long max = 0;
+            int indexMax = -1;
+
+            for (int j = auxIndex; j <= numeroEnteroInt.length - (12 - i); j++) {
+                if (numeroEnteroInt[j] > max) {
+                    max = numeroEnteroInt[j];
+                    indexMax = j;
+                }
+            }
+
+            result = result * 10 + max;
+            auxIndex = indexMax + 1;
+        }
+
+        return result;
+
     }
 }
